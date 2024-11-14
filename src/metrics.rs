daceff7a277e4d71c2a6d118913cc064fcad048f
sam sang <linming1007@gmail.com>
2024-11-14 11:46:42 +0800

    feat: support dashmap

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,13 +1,11 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use core::fmt;
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-};
+use dashmap::DashMap;
+use std::sync::Arc;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>,
 }
 
 impl Default for Metrics {
@@ -18,10 +16,9 @@ impl Default for Metrics {
 
 impl fmt::Display for Metrics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self.data.read().map_err(|_| fmt::Error {})?;
         write!(f, "{{")?;
-        for (key, value) in data.iter() {
-            write!(f, "{}:{}", key, value)?;
+        for entry in self.data.iter() {
+            write!(f, "{}:{}", entry.key(), entry.value())?;
         }
         write!(f, "}}")?;
         Ok(())
@@ -31,29 +28,19 @@ impl fmt::Display for Metrics {
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let counter = data.entry(key.into()).or_insert(0);
+        let mut counter = self.data.entry(key.into()).or_insert(0);
         *counter += 1;
         Ok(())
     }
 
     pub fn dec(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
-        let counter = data.entry(key.into()).or_insert(0);
-        *counter += 1;
+        let mut counter = self.data.entry(key.into()).or_insert(0);
+        *counter -= 1;
         Ok(())
-    }
-
-    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        Ok(self
-            .data
-            .read()
-            .map_err(|e| anyhow!(e.to_string()))?
-            .clone())
     }
 }
